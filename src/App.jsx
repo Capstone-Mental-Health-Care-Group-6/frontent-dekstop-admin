91eb3fe94ea2e1219cec18da25b55ceaac11b64e
karimDev <kromosom06@gmail.com>
2023-11-10 22:41:38 +0700

    styling fragments card

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Sidebar from "./components/Fragments/Sidebar/Sidebar";
+
 // // import "./App.css";
 
 function App() {
-
   return (
     <>
-    <Sidebar/>
+      <Sidebar />
     </>
   );
 }
--- src/components/Fragments/card/Card.jsx
+++ src/components/Fragments/card/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import cardIcon from "../../../assets/Frame 1000003348.svg";
+import "./Card.css";
 
 const Card = ({ src, alt, cardSubtitle, cardTitle }) => {
   return (
@@ -8,13 +9,13 @@ const Card = ({ src, alt, cardSubtitle, cardTitle }) => {
       style={{ width: "243px", height: "178px" }}
     >
       <div className="card-body">
-        <img src={src} alt={alt} className="mb-4" />
+        <img src={src} alt={alt} className="" />
 
         <h6 className="card-subtitle mb-2 text-body-secondary fw-normal ">
           {cardSubtitle}
         </h6>
 
-        <h5 className="card-title ">{cardTitle}</h5>
+        <h5 className="card-title">{cardTitle}</h5>
       </div>
     </div>
   );
