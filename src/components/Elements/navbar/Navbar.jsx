5e2fcc6ed6225d8b11a17e55fd0191b8666559e9
karimDev <kromosom06@gmail.com>
2023-11-15 11:41:11 +0700

    update: title in navbar

--- src/components/Elements/navbar/Navbar.jsx
+++ src/components/Elements/navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import "./Navbar.style.css"
+import "./Navbar.style.css";
 import { IoNotifications } from "react-icons/io5";
 
-const Navbar = () => {
+const Navbar = ({ title }) => {
   return (
-    <nav id="navbar" className="navbar bg-transparent sticky-top d-flex justify-content-end">
-        <div className="">
-           <IoNotifications size={50} className="notif-icon" />
-            <img id="iconNavbar" src="icon-face.png" alt="" />
-          </div>
+    <nav
+      id="navbar"
+      className="navbar bg-transparent sticky-top d-flex align-items-center justify-content-beetwen"
+    >
+      <h1 className="fw-semibold">{title}</h1>
+      <div className="">
+        <IoNotifications size={50} className="notif-icon" />
+        <img id="iconNavbar" src="icon-face.png" alt="" />
+      </div>
     </nav>
   );
 };
