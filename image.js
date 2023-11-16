65b4449ea476f1750e2636048c32e552f060368d
karimDev <kromosom06@gmail.com>
2023-11-16 15:52:03 +0700

    slicing dashboard admin

--- image.js
+++ image.js
@@ -1,5 +1,18 @@
-import logoEmpathiCare from "./src/assets/Logo-brand.svg"
-import logoEmpathiCareMobile from "./src/assets/logomobile.svg"
+import logoEmpathiCare from "./src/assets/Logo-brand.svg";
+import logoEmpathiCareMobile from "./src/assets/logomobile.svg";
+import dashboardImg from "./src/assets/customer-support 1.png";
+import iconCardUser from "./src/assets/Frame 1000003348.svg";
+import iconCardArtikel from "./src/assets/iconArtikel.svg";
+import iconCardDokter from "./src/assets/icon-card-dokter.svg";
+import iconInstan from "./src/assets/1st.svg";
+import iconPremium from "./src/assets/2nd.svg";
 export {
-    logoEmpathiCare, logoEmpathiCareMobile
-}
+  logoEmpathiCare,
+  logoEmpathiCareMobile,
+  dashboardImg,
+  iconCardUser,
+  iconCardArtikel,
+  iconCardDokter,
+  iconInstan,
+  iconPremium,
+};
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,90 @@
 import React from "react";
 import Layouts from "../../Layouts/Layouts";
+import "./Dashboard.styles.css";
+import {
+  dashboardImg,
+  iconCardUser,
+  iconCardArtikel,
+  iconCardDokter,
+  iconInstan,
+  iconPremium,
+} from "../../../image";
+import Card from "../../components/Fragments/card/Card";
 
 const Dashboard = () => {
-  return <Layouts titlePage={"Dashboard"}>Dashboard</Layouts>;
+  return (
+    <Layouts titlePage={"Dashboard"}>
+      <div className="row">
+        <div className="col-12">
+          <div className="wrapper__card">
+            <div className="row d-flex align-items-center">
+              <div className="col-6">
+                <h4 className="fw-semibold text__title mb-4">
+                  Selamat Datang Kembali,Admin!
+                </h4>
+                <p className="text__subtitle">
+                  Senang melihat Anda kembali. Mari kita mulai hari ini dengan
+                  mengelola situs ini.
+                </p>
+              </div>
+              <div className="col-6">
+                <img src={dashboardImg} alt="img-dashboard" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row mt-3">
+        {/* Kolom 1 - Total User dan Total Dokter */}
+        <div className="col-6 d-flex flex-column gap-3">
+          <div className="d-flex gap-3">
+            <Card
+              cardSubtitle="Total User"
+              cardTitle="17.000"
+              src={iconCardUser}
+            />
+            <Card
+              cardSubtitle="Total Dokter"
+              cardTitle="17.000"
+              src={iconCardDokter}
+            />
+          </div>
+
+          <div className="wrapper__artikel">
+            <div className="card-body">
+              <img src={iconCardArtikel} alt="icon-artikel" />
+              <h6 className="card-subtitle mb-2 text-body-secondary fw-normal">
+                Total Artikel
+              </h6>
+              <h5 className="card-title">17.000</h5>
+            </div>
+          </div>
+        </div>
+
+        {/* Kolom 2 - Paket Teratas */}
+        <div className="col-6">
+          <div className="wrapper__paketKonseling ">
+            <h5 className="fw-bold mb-4">Paket Teratas</h5>
+
+            <div className="konseling__instant">
+              <div className="body__konseling d-flex align-items-center">
+                <img src={iconInstan} alt="" />
+                <p className="fw-semibold ms-3">Paket Konseling</p>
+              </div>
+            </div>
+
+            <div className="konseling__premium mt-3">
+              <div className="body__konseling d-flex align-items-center">
+                <img src={iconPremium} alt="" />
+                <p className="fw-semibold ms-3">Paket Konseling</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layouts>
+  );
 };
 
 export default Dashboard;
