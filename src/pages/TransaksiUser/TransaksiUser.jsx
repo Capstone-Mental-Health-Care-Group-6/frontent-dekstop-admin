0bfdc4401bb0b2a09bbb95e42e223eab8504b22c
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-11-16 16:02:16 +0700

    cleancode paket teratas

--- src/pages/TransaksiUser/TransaksiUser.jsx
+++ src/pages/TransaksiUser/TransaksiUser.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import { cardTransaksiUser, riwayatTransaksi1 } from "../../components/DataComponents/dataComponents";
 import RiwayatTransaksi from "../../components/Fragments/riwayat-transaksi-user/RiwayatTransaksi";
 import Button from "../../components/Elements/button/Button";
+import PaketTeratas from "../../components/Fragments/paket-teratas/PaketTeratas";
 
 
 const TransaksiUser = () => {
@@ -28,22 +29,8 @@ const TransaksiUser = () => {
         <div className="col">
           <div className="paket-teratas p-4">
             <h4>Paket Teratas</h4>
-            <div className="paket-1 d-flex gap-2 align-items-center" >
-              <img src={paket1} alt="" />
-              <div className="d-grid align-items-center">
-                <h5> Paket Konseling</h5>
-                <span>Instant</span>
-              </div>
-            </div>
-
-            <div className="paket-2 d-flex gap-2 align-items-center" >
-              <img src={paket2} alt="" />
-              <div className="d-grid align-items-center">
-                <h5> Paket Konseling</h5>
-                <span>Premium</span>
-              </div>
-            </div>
-
+            <PaketTeratas className={'paket-1 d-flex gap-2 align-items-center'} title={'Paket Konseling'} text={'Instant'} image={paket1} />
+            <PaketTeratas className={'paket-2 d-flex gap-2 align-items-center'} title={'Paket Konseling'} text={'Premium'} image={paket2} />
           </div>
         </div>
 
