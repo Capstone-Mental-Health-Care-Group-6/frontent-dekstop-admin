2daabd3507f1321789dc271e6aaa918af5cfaa1c
labit-riset-umm <71822965+alifftwa@users.noreply.github.com>
2023-11-20 20:02:30 +0700

    delete all

--- src/pages/ManageUser/ManageUser.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layouts from "../../Layouts/Layouts";
-
-const ManageUser = () => {
-  return <Layouts titlePage={"Manage User"}>ManageUser</Layouts>;
-};
-
-export default ManageUser;
--- src/pages/PaketKonseling/PaketKonseling.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layouts from "../../Layouts/Layouts";
-
-const PaketKonseling = () => {
-  return <Layouts titlePage={"Paket Konseling"}>PaketKonseling</Layouts>;
-};
-
-export default PaketKonseling;
