6e71d26b977014976939afb1cb76bac2da1da65c
karimDev <kromosom06@gmail.com>
2023-11-30 11:15:39 +0700

    add: handlel eror form in edit profile

--- src/components/Elements/input/Input.jsx
+++ src/components/Elements/input/Input.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Input = ({ type, className, id, onChange, name, placeholder, value }) => {
   return (
     <input
+      required
       type={type ? type : "text"}
       className={`form-control ${className}`}
       id={id}
