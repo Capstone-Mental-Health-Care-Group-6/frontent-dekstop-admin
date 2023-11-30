69046b7eb0675ac90849d3b396e96eba8872e0a8
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-11-30 15:04:13 +0700

    create complex alert

--- src/pages/PaketKonseling/PaketKonseling.jsx
+++ src/pages/PaketKonseling/PaketKonseling.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Layouts from "../../Layouts/Layouts";
 import { BsPencil, BsPlus, BsThreeDots } from "react-icons/bs";
 import "./PaketKonseling.style.css";
-import { defaultImageKonseling, iconPencil } from "../../../image";
+import { alertMessageBlue, defaultImageKonseling, iconPencil } from "../../../image";
 import { dataCardKonsultasi } from "../../components/DataComponents/dataComponents";
 import ModalAlert from "../../components/Fragments/modal-alert/ModalAlert";
 import Button from '../../components/Elements/button/Button'
@@ -42,7 +42,7 @@ const PaketKonseling = () => {
       image: null,
       nama_paket: '',
       harga: '',
-      sesi: '',
+      jumlah_sesi: '',
       keterangan_paket: ''
     })
   }
@@ -100,13 +100,12 @@ const PaketKonseling = () => {
                   />
                 </div>
                 <div className="d-flex gap-2 float-end">
-                  <Button className={'btn'} text="Batal" />
+                  <Button className={'btn'} text="Batal" bsTogle={'modal'} bsTarget={'#modal-batal-edit'} />
                   <Button className={'btn bg-primary text-white'} text="Simpan" />
                 </div>
               </form>
             </ModalAlert>
           </div>
-
         </section  >
 
         <section className="all-konseling mt-4" >
@@ -136,7 +135,7 @@ const PaketKonseling = () => {
 
                   <ModalAlert id={'modal-edit'} >
                     <div className="d-flex justify-content-between p-3 text-black fw-semibold">
-                      Paket Konseling Premium
+                      Edit Paket Konseling Premium
                       <Button className={'btn-close border-0 '} bsDismiss={'modal'} />
                     </div>
 
@@ -175,7 +174,7 @@ const PaketKonseling = () => {
                         />
                       </div>
                       <div className="d-flex gap-2 float-end">
-                        <Button className={'btn'} text="Batal" />
+                        <Button className={'btn'} text="Batal" bsTogle={'modal'} bsTarget={'#modal-batal-edit'} />
                         <Button className={'btn bg-primary text-white'} text="Simpan" />
                       </div>
                     </form>
@@ -190,6 +189,22 @@ const PaketKonseling = () => {
               </div>
             ))}
 
+            <ModalAlert id={'modal-batal-edit'} >
+              <div className="alert-modal-batal-edit p-3">
+                <div className="d-flex flex-column justify-content-center align-items-center mb-3">
+                  <img className='mb-4 mt-4' src={alertMessageBlue} alt="" />
+                  <div className="text-center">
+                    <h5 className="fw-bold" >Batalkan Tambah Data?</h5>
+                    <span>Tindakan ini akan membuat data yang Anda masukan menjadi tidak tersimpan</span>
+                  </div>
+                </div>
+
+                <div className="d-flex gap-2 justify-content-end">
+                  <Button className={'btn bg-primary text-white fw-medium'} text="Batal" bsTogle={'modal'} bsTarget={'#modal-edit'} />
+                  <Button className={'btn  text-primary border-primary fw-medium'} bsDismiss={'modal'} text="Ya" onClick={deleteState} />
+                </div>
+              </div>
+            </ModalAlert>
 
           </div>
         </section>
