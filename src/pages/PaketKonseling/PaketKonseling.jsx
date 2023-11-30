e63a23fc8bba6cb147da8746e0c2b63a5d7762c9
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-11-30 13:57:39 +0700

    create modal edit

--- src/pages/PaketKonseling/PaketKonseling.jsx
+++ src/pages/PaketKonseling/PaketKonseling.jsx
@@ -119,7 +119,7 @@ const PaketKonseling = () => {
                   <div className="card-header border-0">
                     <div className="d-flex align-items-center justify-content-between">
                       <div className=" d-flex gap-3 align-items-center ">
-                        <img src={item.image} />
+                        <img className="image-konsultasi" src={item.image} />
                         <div>
                           <h6 className="m-0" >{item.paket}</h6>
                           <h5 className="m-0 fw-bold" >{item.sesi}</h5>
@@ -127,11 +127,59 @@ const PaketKonseling = () => {
                       </div>
                       <button className="btn border-0" data-bs-toggle="dropdown">{<BsThreeDots />}</button>
                       <ul className="dropdown-menu px-1">
-                        <li > <button className="btn w-100 fw-semibold mb-2" > Edit </button> </li>
-                        <li > <button className="btn w-100 fw-semibold "  > Hapus </button> </li>
+                        <li > <button className="btn w-100 fw-semibold mb-2" data-bs-toggle="modal" data-bs-target="#modal-edit" > Edit </button> </li>
+                        <li > <button className="btn w-100 fw-semibold " > Hapus </button> </li>
                       </ul>
+
                     </div>
                   </div>
+
+                  <ModalAlert id={'modal-edit'} >
+                    <div className="d-flex justify-content-between p-3 text-black fw-semibold">
+                      Paket Konseling Premium
+                      <Button className={'btn-close border-0 '} bsDismiss={'modal'} />
+                    </div>
+
+                    <form onSubmit={handleSubmit} className="p-3">
+
+                      <div className="position-relative mx-auto">
+                        <div className="image d-flex justify-content-center ">
+                          {formData.image ? (
+                            <img src={URL.createObjectURL(formData.image)} />
+                          ) : (
+                            <img src={defaultImageKonseling} />
+                          )}
+                          <input
+                            type="file"
+                            className="d-none"
+                            id="imageInput"
+                            onChange={handleImageChange}
+                          />
+                          <button className="btn border-0 position-absolute bottom-0 end-0" type="button" ><img src={iconPencil} onClick={handleFileManager} /></button>
+                        </div>
+                      </div>
+
+                      <InputForm htmlFor={'nama_paket'} value={formData.nama_paket} title={'Nama Paket'} placeholder={'Masukan Nama'} onChange={handleChange} />
+                      <InputForm htmlFor={'harga'} value={formData.harga} title={'Harga'} placeholder={'Masukan Harga'} onChange={handleChange} />
+                      <InputForm htmlFor={'jumlah_sesi'} value={formData.jumlah_sesi} title={'Banyak Sesi'} placeholder={'Masukan Jumlah Sesi'} onChange={handleChange} />
+
+                      <label htmlFor="keterangan_paket" className="fw-semibold mt-2 mb-2" >Keterangan Paket</label>
+                      <div className="form-floating ">
+                        <textarea
+                          className="form-control"
+                          placeholder="Masukan Penjelasan Paket"
+                          style={{ height: 100 }}
+                          name="keterangan_paket"
+                          value={formData.keterangan_paket}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="d-flex gap-2 float-end">
+                        <Button className={'btn'} text="Batal" />
+                        <Button className={'btn bg-primary text-white'} text="Simpan" />
+                      </div>
+                    </form>
+                  </ModalAlert>
 
                   <div className="card-body">
                     <span>
