c9a71db1dcc954ce4ed55b300aa10edc19c3a6d5
Alif Fatwa Ramadhani <71822965+alifftwa@users.noreply.github.com>
2023-12-01 18:16:07 +0700

    Delete src directory

--- src/pages/DanaDokter/DanaDokter.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layouts from "../../Layouts/Layouts";
-
-const DanaDokter = () => {
-  return <Layouts titlePage={"Dana Dokter"}>dana dokter</Layouts>;
-};
-
-export default DanaDokter;
