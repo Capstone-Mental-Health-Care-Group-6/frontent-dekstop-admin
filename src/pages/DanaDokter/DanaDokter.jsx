b18067a1d07486844eb24fdb841e9c1483e53c52
karimDev <kromosom06@gmail.com>
2023-12-19 16:18:23 +0700

    add: skeleton loading

--- src/pages/DanaDokter/DanaDokter.jsx
+++ src/pages/DanaDokter/DanaDokter.jsx
@@ -7,16 +7,19 @@ import { dataTableDanaDokter } from "../../components/DataComponents/dataCompone
 import { searchFailed } from "../../../image";
 import { getAllWithDraw } from "../../service/withDraw";
 import { useLogin } from "../../hooks/useLogin";
+import Skeleton from "react-loading-skeleton";
 
 const DanaDokter = () => {
   useLogin();
   const [searchValue, setSearchValue] = useState(""); // State untuk nilai pencarian
-
   const [dataTableDanaDokter, setDataTableDanaDokter] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     getAllWithDraw((data) => {
       setDataTableDanaDokter(data.data);
+      setLoading(false);
     });
   }, []);
 
@@ -37,23 +40,27 @@ const DanaDokter = () => {
           />
         </div>
 
-        {dataTableDanaDokter.length === 0 ? (
-          <div className="dana__dokter__kosong d-flex align-items-center justify-content-center flex-column">
-            <img src={searchFailed} alt="img-pasien-kosong" />
-            <h5 className="fw-semibold">
-              Tidak ada data pencairan dana dokter
-            </h5>
-            <p>
-              Maaf, Saat ini belum ada data request pencairan dana dokter yang
-              tersedia. Ini bisa jadi karena belum ada dokter yang mengajukan
-              pencairan dana atau data pencairan sedang dimuat.
-            </p>
-          </div>
+        {!loading ? (
+          dataTableDanaDokter.length === 0 ? (
+            <div className="dana__dokter__kosong d-flex align-items-center justify-content-center flex-column">
+              <img src={searchFailed} alt="img-pasien-kosong" />
+              <h5 className="fw-semibold">
+                Tidak ada data pencairan dana dokter
+              </h5>
+              <p>
+                Maaf, Saat ini belum ada data request pencairan dana dokter yang
+                tersedia. Ini bisa jadi karena belum ada dokter yang mengajukan
+                pencairan dana atau data pencairan sedang dimuat.
+              </p>
+            </div>
+          ) : (
+            <TablePencairanSaldo
+              data={dataTableDanaDokter}
+              searchValue={searchValue}
+            />
+          )
         ) : (
-          <TablePencairanSaldo
-            data={dataTableDanaDokter}
-            searchValue={searchValue}
-          />
+          <Skeleton height={60} count={6} />
         )}
       </section>
     </Layouts>
